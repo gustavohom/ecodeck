def2d7b6e43005e24167f15f60fa9103a19ae140
Gustavo Mourao <49279336+gustavohom@users.noreply.github.com>
2024-08-23 21:58:41 -0300

    Create cartasComplexas.js

---
+++ src/components/cartasComplexas.js
@@ -0,0 +1,21 @@
+const cartasComplexas = [
+  {
+    tipo: "MultiplaEscolha",
+    titulo: "Complexa - Pergunta 1",
+    pergunta: "carta complexa",
+    opcoes: [
+      { id: 1, texto: "Econômico, social e ambiental" },
+      { id: 2, texto: "Político, social e cultural" },
+      { id: 3, texto: "Econômico, tecnológico e ambiental" },
+    ],
+    respostaCorreta: [1],
+    dificuldade: "dificil",
+    categorias: ["Sustentabilidade"],
+    fontes: ["Fonte 2"],
+    vantagem: "Você avança 2 casas.",
+    desvantagem: "Você retrocede 1 casa.",
+    dica: "Pense em aspectos que afetam o planeta e a sociedade.",
+  },
+];
+
+export default cartasComplexas;
