8756fc6d97ce1b1fe12cc5651d5570130c8c3f5e
Gustavo Mourao <49279336+gustavohom@users.noreply.github.com>
2024-10-04 18:40:38 -0300

    Update estrelas_aliens.js

--- src/components/estrelas_aliens.js
+++ src/components/estrelas_aliens.js
@@ -758,7 +758,7 @@ const estrelas_aliens = [
   {
     tipo: "Desvantagem",
     titulo: "Buraco Negro",
-    pergunta: "Nada pode escapar do meu alcance, jogador. Você terá que sacrificar algo para sobreviver. O que será? Sua decisão pode mudar tudo."",
+    pergunta: "Nada pode escapar do meu alcance, jogador. Você terá que sacrificar algo para sobreviver. O que será? Sua decisão pode mudar tudo.",
     opcoes: [
       { id: 1, texto: "Todos os jogadores recuam o número de casas igual ao número de estrelas que possuem. Se alguém não possuir  estrelas, ele deve reiniciar todos os seus contadores." },
     ],
