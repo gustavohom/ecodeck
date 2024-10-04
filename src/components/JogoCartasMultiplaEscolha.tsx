ca106d8998b54a39dd3dd739109922511d84491e
Gustavo Mourao <49279336+gustavohom@users.noreply.github.com>
2024-10-04 18:34:27 -0300

    Update JogoCartasMultiplaEscolha.tsx

--- src/components/JogoCartasMultiplaEscolha.tsx
+++ src/components/JogoCartasMultiplaEscolha.tsx
@@ -33,9 +33,10 @@ import "react-medium-image-zoom/dist/styles.css";
 import Image from "next/image";
 import cartasSimples from "./cartas";
 import cartasComplexas from "./cartas_complexas";
+import estrelasAliens from "./estrelas_aliens";
 
 // Combine as cartas simples e complexas em um único array
-const cartas = [...cartasSimples, ...cartasComplexas];
+const cartas = [...cartasSimples, ...cartasComplexas, ...estrelasAliens];
 
 // Definição de Tipos
 interface Opcao {
