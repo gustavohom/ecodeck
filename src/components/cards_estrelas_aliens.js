2661a706d1b7de538ec884a8dd5cbab267cabe97
Gustavo Mourao <49279336+gustavohom@users.noreply.github.com>
2024-10-09 14:29:18 -0300

    Update and rename estrelas_aliens.js to cards_estrelas_aliens.js

--- src/components/cards_estrelas_aliens.js
+++ src/components/cards_estrelas_aliens.js
@@ -9,7 +9,7 @@ const estrelas_aliens = [
     ],
     respostaCorreta: [1],
     dificuldade: "facil",
-    categorias: ["Teste Estrelas vs Aliens"],
+    categorias: ["Estrelas vs Aliens"],
     fontes: ["Imagens: Fonte na descrição do jogo"],
     vantagem: "",
     desvantagem: "",
