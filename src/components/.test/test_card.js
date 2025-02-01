378dc117a99dfdf12285bb45121945582202a714
Gustavo Mourao <49279336+gustavohom@users.noreply.github.com>
2025-02-01 17:56:31 -0300

    Update test_card.js

--- src/components/.test/test_card.js
+++ src/components/.test/test_card.js
@@ -1,4 +1,5 @@
-const meu_baralho = [
+const test_card = [
+ [
   {
     "tipo": "Pergunta",
     "titulo": "Pergunta com Imagem",
@@ -275,4 +276,4 @@ const meu_baralho = [
   }
 ];
 
-export default meu_baralho;
+export default test_card;
