18a06cec44f8f1426ee602f66f0d600365b5a845
Gustavo Mourao <gustavohen1@gmail.com>
2025-04-22 13:10:19 -0300

    Update CriadorDeCarta.tsx

--- src/components/CriadorDeCarta.tsx
+++ src/components/CriadorDeCarta.tsx
@@ -142,7 +142,7 @@ const CardStaticView: React.FC<{ card: Partial<Carta> }> = ({ card }) => {
             break;
         case "CompletarFrase":
             const cardCompFrase = card as Partial<CartaCompletarFrase>;
-            renderedSpecifics = <p className="text-xs mt-1 italic">Frase: "{cardCompFrase.fraseIncompleta || '...'}"</p>;
+            renderedSpecifics = <p className="text-xs mt-1 italic">Frase: &quot;{cardCompFrase.fraseIncompleta || '...'}&quot;</p>;
             renderedOptions = (
                 <>
                  {cardCompFrase.fragmentos && cardCompFrase.fragmentos.length > 0 && (
